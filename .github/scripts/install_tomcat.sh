d11d310638c759bd71e0ab53ded1af7d745806c9
Nicolas <Nicolas Rousset>
2021-05-27 09:27:12 +0200

    Changing Tomcat version

--- .github/scripts/install_tomcat.sh
+++ .github/scripts/install_tomcat.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
+TOMCAT_VERSION='10.0.6'
 
-curl https://downloads.apache.org/tomcat/tomcat-10/v10.0.5/bin/apache-tomcat-10.0.5.tar.gz -o /tmp/apache-tomcat-10.0.5.tar.gz
+curl "https://downloads.apache.org/tomcat/tomcat-10/v$TOMCAT_VERSION/bin/apache-tomcat-$TOMCAT_VERSION.tar.gz" -o /tmp/apache-tomcat-10.tar.gz
 mkdir /opt/tomcat
 tar xzf /tmp/apache-tomcat*tar.gz -C /opt/tomcat --strip-components=1
