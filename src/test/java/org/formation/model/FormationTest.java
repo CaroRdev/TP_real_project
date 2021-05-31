f4b474d8825f660c26b381087e12d84b2bcdba04
Nicolas <Nicolas Rousset>
2021-05-31 21:43:08 +0200

    Correcting Junit

--- src/test/java/org/formation/model/FormationTest.java
+++ src/test/java/org/formation/model/FormationTest.java
@@ -6,14 +6,14 @@ public class FormationTest extends TestCase {
 	public void testToString()
 	{     
         assertEquals(
-        	"Formation;20210528;NULL;1;300;active;original;NULL",
+        	"Formation;20210528;NULL;1;300;Active;Original;NULL",
         	(new Formation(
         		"20210528", 
         		null, 
         		null,
         		300,
-        		"active",
-        		"original",
+        		"Active",
+        		"Original",
         		1
         	)).toString()
         );
