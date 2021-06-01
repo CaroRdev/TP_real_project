01ea29bdd8bfaa5a8134696355b920ee5ab7d457
zohra-20 <zohra.moutaouakkil.j@gmail.com>
2021-06-01 12:19:55 +0200

    task 3

--- src/main/java/org/formation/model/Fixture.java
+++ src/main/java/org/formation/model/Fixture.java
@@ -45,6 +45,8 @@ public class Fixture {
 		rogue.getSujets().add(divination);	
 		rogue.getSujets().add(dfo);
 		
+		Formateur trelawney = addFormateur("Trelawney", 4);
+		
 		Formation dfoFormation = addFormation(
 				"20210608",
 				dfo,
@@ -61,6 +63,16 @@ public class Fixture {
 				rogue,
 				300,
 				"Active",
+				"Original",
+				2
+		);
+		
+		Formation divinationFormation = addFormation(
+				"20211031",
+				null,
+				null,
+				300,
+				"Fusionnee",
 				"Original",
 				2
 		);
@@ -101,4 +113,6 @@ public class Fixture {
 		
 		return newFormateur;
 	}
+	
+	
 }
--- src/test/java/org/formation/model/FormationTest.java
+++ src/test/java/org/formation/model/FormationTest.java
@@ -1,6 +1,8 @@
 package org.formation.model;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import junit.framework.TestCase;
 
@@ -25,6 +27,7 @@ public class FormationTest extends TestCase {
 		// What we are mainly testing here is that the method call doesn't raise
 		// an error
 		assertTrue(Formation.getAll() instanceof List<?>);
+		assertEquals(1,Formation.getAll().stream().filter(p -> p.getStatut().equals("Fusionnee")).collect(Collectors.toList()).size());
 	}
 	
 	public void testIsFusionnee() {
