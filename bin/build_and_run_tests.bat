8385d90084f4bc0e13607e0ea3ef2fa9851adb55
CaroRdev8 <carolinerolloy.dev@mail.com>
2021-06-01 11:21:04 +0200

    delate bin

--- bin/build_and_run_tests.bat
+++
@@ -1,2 +0,0 @@
-docker build . -t jakartaee
-docker run jakartaee bash -c "bash test_as_shell/run_tomcat.sh > /dev/null && bash test_as_shell/test_all.sh"
--- bin/build_and_run_tests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker build . -t jakartaee
-docker run --name jakartaee_test jakartaee bash -c "bash test_as_shell/run_tomcat.sh > /dev/null && bash test_as_shell/test_all.sh"
-docker container rm jakartaee_test
