e684d3abf0af25a5a6ea74c1e0b1776cf98fb903
lgentaz <lucile.gentaz@gmail.com>
2021-06-01 17:06:52 +0200

    add annulationbean

--- src/main/java/org/formation/bean/AnnulationBean.java
+++ src/main/java/org/formation/bean/AnnulationBean.java
@@ -1,14 +1,14 @@
 package org.formation.bean;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.formation.model.Formation;
 
 public class AnnulationBean {
 
   public List<Formation> selectAnnulation() {
-    Formation.getAll().stream().filter((s) -> s.isAnnulee());
-    return null;
+    return Formation.getAll().stream()
+    .filter((s) -> s.isAnnulee()).collect(Collectors.toList());
   }
-  
 }
